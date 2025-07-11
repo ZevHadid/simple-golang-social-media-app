f1cf7bba3bf0ac00c1fcfce9fb6710a902718691
ZevHadid <zevhadid@gmail.com>
2025-07-11 18:24:13 +0700

    Added function for handling unauthorized access to routes

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -11,17 +11,22 @@ func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tokenString, err := c.Cookie("token")
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			handleUnauthorized(c, "no-auth-token-found")
 			return
 		}
 
 		email, err := utils.ValidateJWT(tokenString)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			handleUnauthorized(c, "invalid-auth-token")
 			return
 		}
 
 		c.Set("email", email)
 		c.Next()
 	}
+}
+
+func handleUnauthorized(c *gin.Context, reason string) {
+	c.Redirect(http.StatusFound, "/login?reason="+reason)
+	c.Abort()
 }
