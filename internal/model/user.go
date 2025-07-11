c4837b816ad30aed29071de2106f63a46e269076
ZevHadid <zevhadid@gmail.com>
2025-07-11 23:09:44 +0700

    Updated user model to have user id

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,7 @@
 package model
 
 type User struct {
+	ID       uint   `gorm:"primaryKey"`
 	Username string `json:"username"`
 	Email    string `gorm:"unique" json:"email"`
 	Password string `json:"-"`
